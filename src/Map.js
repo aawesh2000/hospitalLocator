0318530fbdb10bb9967c03b1769d0f4ba57b6e54
MD Aawesh <aawesh2000@gmail.com>
2022-04-09 20:55:57 +0530

    Title Changes

--- src/Map.js
+++ src/Map.js
@@ -95,15 +95,6 @@ More at: http://covidhospitals.online/`,
 
         return (<MapContainer center={center} zoom={5} whenCreated={this.mapCreated}>
 
-           
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright" target="_blank" rel="noreferrer">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <TileLayer
-                attribution='Made with ❤️ by Aawesh & Saurabh'
-            />
-
             
             <LocateControl key="locate" options={locateOptions} />
             <SearchControl />
