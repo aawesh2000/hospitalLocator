9afe9ac1028a0892b14673b7b0aa676d85a11ff7
MD Aawesh <aawesh2000@gmail.com>
2022-04-10 00:39:31 +0530

    project related changes

--- src/components/RightControls.js
+++ src/components/RightControls.js
@@ -110,10 +110,9 @@ function SourceDialog(props) {
                 </TableContainer>
 
                 <p>
-                    This site is an open source project created by <a href="https://www.linkedin.com/in/md-aawesh-patanwala-3178a3166/" target="_blank" >Md Aawesh</a>.
+                    This site is created by <a href="https://www.linkedin.com/in/md-aawesh-patanwala-3178a3166/" target="_blank" >Md Aawesh</a>.
                     </p>
-                <p> Please feel make changes to this, or add more data sources at <a href="https://www.linkedin.com/in/md-aawesh-patanwala-3178a3166/" target="blank">https://www.linkedin.com/in/md-aawesh-patanwala-3178a3166/</a>
-                </p>
+                
             </DialogContent>
             <DialogActions>
                 <Button autoFocus onClick={handleClose} color="primary">OK</Button>
