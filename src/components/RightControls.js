61f2b6f1a0e756614b15c2ffcf08d11af7e6b8d0
MD Aawesh <aawesh2000@gmail.com>
2022-04-09 23:27:57 +0530

    Title Changes3.o

--- src/components/RightControls.js
+++ src/components/RightControls.js
@@ -110,9 +110,9 @@ function SourceDialog(props) {
                 </TableContainer>
 
                 <p>
-                    This site is an open source project created by <a href="https://github.com/pgollangi" target="_blank" >pgollangi</a>.
+                    This site is an open source project created by <a href="https://www.linkedin.com/in/md-aawesh-patanwala-3178a3166/" target="_blank" >pgollangi</a>.
                     </p>
-                <p> Please feel make changes to this, or add more data sources at <a href="https://github.com/covidhospitals/covidhospitals" target="blank">github.com/covidhospitals/covidhospitals</a>
+                <p> Please feel make changes to this, or add more data sources at <a href="https://www.linkedin.com/in/md-aawesh-patanwala-3178a3166/" target="blank">https://www.linkedin.com/in/md-aawesh-patanwala-3178a3166/</a>
                 </p>
             </DialogContent>
             <DialogActions>
